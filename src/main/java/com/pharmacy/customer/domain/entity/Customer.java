af2fb2258f11b3167b30c7f520565bd2b7936c8d
juanrojasaguilar <yo217@hotmail.es>
2024-08-14 09:03:06 -0500

    feat

--- src/main/java/com/pharmacy/customer/domain/entity/Customer.java
+++ src/main/java/com/pharmacy/customer/domain/entity/Customer.java
@@ -1,6 +1,7 @@
 package com.pharmacy.customer.domain.entity;
 
 import java.sql.Date;
+import java.util.Calendar;
 
 public class Customer {
   private int id;
@@ -48,4 +49,50 @@ public class Customer {
   public void setLastName(String lastName) {
     this.lastName = lastName;
   }
+
+  public int getIdCity() {
+    return idCity;
+  }
+
+  public void setIdCity(int idCity) {
+    this.idCity = idCity;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public Date getBirthDate() {
+    return birthDate;
+  }
+
+  public java.sql.Date getSqlDate() {
+    int year = birthDate.valueOf("y");
+    java.sql.Date sqlDate = new java.sql.Date(id, id, id);
+  }
+
+  public void setBirthDate(Date birthDate) {
+    this.birthDate = birthDate;
+  }
+
+  public double getLongitude() {
+    return longitude;
+  }
+
+  public void setLongitude(double longitude) {
+    this.longitude = longitude;
+  }
+
+  public double getLatitude() {
+    return latitude;
+  }
+
+  public void setLatitude(double latitude) {
+    this.latitude = latitude;
+  }
+  
 }
