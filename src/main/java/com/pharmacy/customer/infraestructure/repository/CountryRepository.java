11a65cd78761310bab2c4bf99f0d089151f6374b
JuanRojas <87134849+JuanRojasAguilar@users.noreply.github.com>
2024-08-14 08:24:32 -0500

    Update CostumerRepository.java

--- src/main/java/com/pharmacy/customer/infraestructure/repository/CountryRepository.java
+++ src/main/java/com/pharmacy/customer/infraestructure/repository/CountryRepository.java
@@ -1,7 +1,7 @@
-package com.pharmacy.country.infraestructure.repository;
+package com.pharmacy.customer.infraestructure.repository;
 
-import com.pharmacy.country.domain.entity.Country;
-import com.pharmacy.country.domain.service.CountryService;
+import com.pharmacy.customer.domain.entity.Customer;
+import com.pharmacy.customer.domain.service.CustomerService;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.DriverManager;
@@ -13,10 +13,10 @@ import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
-public class CountryRepository implements CountryService {
+public class CustomerRepository implements CustomerService {
   private Connection connection;
 
-  public CountryRepository() {
+  public CustomerRepository() {
     try {
       Properties props = new Properties();
       props.load(getClass().getClassLoader().getResourceAsStream("db.properties"));
@@ -30,17 +30,23 @@ public class CountryRepository implements CountryService {
   }
 
   @Override
-  public void add(Country country) {
-    String sql = "INSERT INTO countries (name) VALUES (?)";
+  public void add(Customer customer) {
+    String sql = "INSERT INTO customers (first_name, last_name, id_city, email, birth_date, longitude, latitude) VALUES (?,?,?,?,?,?,?)";
 
     try {
       PreparedStatement statement = connection.prepareStatement(sql, PreparedStatement.RETURN_GENERATED_KEYS);
-      statement.setString(1, country.getName());
+      statement.setString(1, customer.getFirstName());
+      statement.setString(2, customer.getLastName());
+      statement.setInt(3, customer.getIdCity());
+      statement.setString(4, customer.getEmail());
+      statement.setDate(5, customer.getSqlDate());
+      statement.setDouble(6, customer.getLongitude());
+      statement.setDouble(7, customer.getLatitude());
       statement.executeUpdate();
 
       try (ResultSet generatedKey = statement.getGeneratedKeys()) {
         if (generatedKey.next()) {
-          country.setId(generatedKey.getInt(1));
+          customer.setId(generatedKey.getInt(1));
         }
       }
     } catch (SQLException e) {
@@ -49,16 +55,23 @@ public class CountryRepository implements CountryService {
   }
 
   @Override
-  public Optional<Country> findById(int id) {
-    String sql = "SELECT * FROM countries WHERE id_country = ?";
+  public Optional<Customer> findById(int id) {
+    String sql = "SELECT * FROM customers WHERE id_customer = ?";
     try {
       PreparedStatement statement = connection.prepareStatement(sql);
       statement.setInt(1, id);
       try (ResultSet result = statement.executeQuery(sql)) {
-        Country country = new Country();
-        country.setId(result.getInt("id_country"));
-        country.setName(result.getString("name"));
-        return Optional.of(country);
+
+        int idCustomer = result.getInt("id_customer");
+        String firstName = result.getString("first_name");
+        String lastName = result.getString("last_name");
+        String email = result.getString("email");
+        Date birthDate = result.getDate("birth_date");
+        double longitude = result.getDouble("longitude");
+        double latitude = result.getDouble("latitude");
+
+        Customer customer = new Customer(idCustomer, firstName, lastName, email, birthDate, longitude, latitude);
+        return Optional.of(customer);
       }
     } catch (SQLException e) {
       e.printStackTrace();
@@ -67,8 +80,8 @@ public class CountryRepository implements CountryService {
   }
 
   @Override
-  public Optional<List<Country>> showAllInstances() {
-    List<Country> countries = new ArrayList<>();
+  public Optional<List<Customer>> showAllInstances() {
+    List<Customer> customers = new ArrayList<>();
     String sql = "SELECT * FROM countries";
     try (PreparedStatement statement = connection.prepareStatement(sql)) {
       ResultSet result = statement.executeQuery(sql);
